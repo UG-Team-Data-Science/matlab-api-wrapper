62465c5afd7f0325882fa24c644234f72d6ee8cc
Herbert Kruitbosch <H.T.Kruitbosch@rug.nl>
2026-03-16 16:04:06 +0100

    integration in home page

--- web/webapp/src/components/layout/Header.js
+++ web/webapp/src/components/layout/Header.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { Container, Grid, Box  } from '@mui/material';
+
+import { Container, Grid, Box, Link } from '@mui/material';
 
 import './Layout.css';
 import "../../rug-huisstijl.css"
 import logo from "../../images/logo--en.png"
 import logo_only from "../../images/logo.gif"
+import {Home as HomeIcon} from "@mui/icons-material";
+import {useLocation} from "react-router-dom";
 
 
 export function Header() {
+  const location = useLocation();
+
   return (<>
     <div key="top-bar" className="bar-shadow hide-sm"><Container maxWidth="xl"><Grid container spacing={0}>
       <Grid size={8}>
@@ -17,9 +22,20 @@ export function Header() {
       <Grid size={4}>
       </Grid>
     </Grid></Container></div>
-    <div key="second-top-bar" className="rug-bar white-red-bg">
-      <Container maxWidth="xl" className="slash-bg" style={{height: "100%", paddingLeft: "80px", color: "white", fontWeight: 900, paddingTop: "7px"}}>
-        My Magic Model
+    <div key="second-top-bar" className="rug-bar">
+      <Container maxWidth="xl" className="slash-bg"
+                 style={{height: "100%", paddingLeft: "80px", color: "white", fontWeight: 900, flexDirection: "row",
+                         display: "flex", justifyContent: "space-between", padding: 0, maxWidth: "100%"}}>
+        <Box style={{flexGrow: 1, padding: 0, lineHeight: "50px", color: "white"}}>
+          <Link href={"/"} key="home-link" className={ location.pathname === "/" ? "active " : "" }>
+            <HomeIcon style={{marginTop: "5px", marginBottom: "-5px"}}/>
+          </Link>
+          &nbsp;My Magic Model
+        </Box>
+        <Box style={{flexGrow: 0, padding: 0, lineHeight: "50px", color: "white"}}>
+          <Link href={"/privacy"} key="privacy-link" className={ location.pathname.startsWith("/privacy") ? "active " : "" }>Privacy</Link>
+          <Link href={"/eula"} key="eula-link" className={ location.pathname.startsWith("/eula") ? "active " : "" }>EULA</Link>
+        </Box>
       </Container>
     </div>
   </>);
